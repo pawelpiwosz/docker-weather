aa2fd2b18e6f67e5f45d9d5146f6e09f58e9e210
Pawel Piwosz <pawel@pawelpiwosz.net>
2019-05-16 23:48:42 +0200

    Add Travis

---
+++ test.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+docker build -t wego .
+
+echo "                       ---                                   "
+echo "Successfully built wego from branch $1.                   "
+echo "Commit $2                                                    "
+echo "                       ---                                   "
