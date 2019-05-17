f92e6a3301ca9c50f8b16304f5567013a8075c4a
Pawel Piwosz <pawel@pawelpiwosz.net>
2019-05-17 16:21:08 +0200

    remove obsolete test file

--- test-run.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-docker_status=$(docker run --rm "$1/weather")
-echo "${docker_status}"
-if [[ $docker_status == *"You have to register for one at"* ]]; then
-    exit 0
-else
-    exit 1
-fi
